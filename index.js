50bb658ccf3db172aa9ec6ae1404ff790e165e66
ErakiJeff <erakiproj99@gmail.com>
2021-04-29 11:13:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require("fs");
+
+app = express();
+const port = 8000;
+
+app.use(express.static('public'));
+app.get("/", (req, res) => {
+    res.sendFile("index.html")
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log("listening on port " + port)
+});
